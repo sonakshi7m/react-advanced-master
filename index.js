25dbec277a7add401d82e2812a4645c3bb68010f
Sonakshi Mediratta <sonakshi.mediratta@onsolve.com>
2020-03-20 13:13:07 +0530

    changed PORT

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
 
-//const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(5000);
+
+//heroku links
+// https://mysterious-meadow-04344.herokuapp.com/ -identifies the name of your application to heroku
+// https://git.heroku.com/mysterious-meadow-04344.git - deployment target. git repo that we can push our locak server to
